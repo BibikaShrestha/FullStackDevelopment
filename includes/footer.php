71fe5ce6e4879e197546dbb54049e77e08bdeb78
BibikaShrestha <np03cs4a240020@heraldcollege.edu.np>
2025-12-21 23:31:59 +0545

    Added files via upload
    Student Portfolio Manager

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<hr>
+<p align="center">
+    &copy; <?php echo date("Y"); ?> Student Portfolio Manager
+</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require "includes/header.php"; ?>
+
+<h3>Welcome</h3>
+<p>This application manages student information and portfolio uploads.</p>
+
+<ul>
+    <li><a href="add_student.php">Add Student Info</a></li>
+    <li><a href="upload.php">Upload Portfolio File</a></li>
+    <li><a href="students.php">View Students</a></li>
+</ul>
+
+<?php require "includes/footer.php"; ?>
